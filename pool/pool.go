c8b8d138e1d174c95f23c511c108cb079a5f1e86
Barry Allen <future.speeder@gmail.com>
2019-08-30 23:34:59 +0800

    rename pool

--- pool/pool.go
+++ pool/pool.go
@@ -2,32 +2,32 @@ package pool
 
 var defaultCapacity int64 = 10
 
-type Pool struct {
+type pool struct {
 	capacity int64
 	cc       chan uint8
 }
 
-func (p *Pool) Get() {
+func (p *pool) Get() {
 	p.cc <- 1
 }
 
-func (p *Pool) Put() {
+func (p *pool) Put() {
 	<-p.cc
 }
 
-func (p *Pool) Cap() int64 {
+func (p *pool) Cap() int64 {
 	return p.capacity
 }
 
-func (p *Pool) Close() {
+func (p *pool) Close() {
 	close(p.cc)
 }
 
-func NewPool(capacity int64) *Pool {
+func NewPool(capacity int64) *pool {
 	if capacity < 0 {
 		capacity = defaultCapacity
 	}
-	return &Pool{
+	return &pool{
 		capacity: capacity,
 		cc:       make(chan uint8, capacity),
 	}
