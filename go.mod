af87db5c6b4cd1a30f08707b375d7dc6e8d442d8
Barry Allen <future.speeder@gmail.com>
2019-08-30 23:35:55 +0800

    update subscriber.go and update go mod

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.12
 
 require (
 	github.com/google/go-cmp v0.3.1 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/google/uuid v1.1.1
+	github.com/pkg/errors v0.8.1
 	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
 	gotest.tools v2.2.0+incompatible
 )
--- subscriber.go
+++ subscriber.go
@@ -1,1 +1,116 @@
 package easy_amqp
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"github.com/speeder-allen/easy-amqp/pool"
+	"github.com/streadway/amqp"
+	"reflect"
+	"sync"
+)
+
+var (
+	defaultConcurrencyCount  int64 = 10
+	ErrorConnection                = errors.New("error connection of amqp")
+	ErrorChanRecvFail              = errors.New("channel recv fail")
+	ErrorEmptySubscribeQueue       = errors.New("subscribe queues empty")
+)
+
+type Handle func(delivery *amqp.Delivery)
+
+type subscriber struct {
+	conn *amqp.Connection
+	ctx  context.Context
+	uuid string
+	sync.Mutex
+	queues map[string]*SubscribeOption
+}
+
+func (s *subscriber) Subscribe(queuename string, option *SubscribeOption) {
+	s.Lock()
+	defer s.Unlock()
+	s.queues[queuename] = option
+}
+
+func (s *subscriber) Comsume(concurrency int64) error {
+	if s.conn == nil {
+		return ErrorConnection
+	}
+	if len(s.queues) == 0 {
+		return ErrorEmptySubscribeQueue
+	}
+	channel, err := s.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
+	if concurrency <= 0 {
+		concurrency = defaultConcurrencyCount
+	}
+	p := pool.NewPool(concurrency)
+	defer p.Close()
+	var fns []Handle
+	var consumes []interface{}
+	consumes = append(consumes, s.ctx.Done())
+	for na, opt := range s.queues {
+		ch, err := channel.Consume(na, s.uuid+"-"+na, opt.AutoAck, opt.Exclusive, opt.NoLocal, opt.NoWait, opt.Args)
+		if err != nil {
+			return err
+		}
+		consumes = append(consumes, ch)
+		fns = append(fns, opt.Hande)
+	}
+	cases := make([]reflect.SelectCase, len(consumes))
+	for idx := 0; idx < len(consumes); idx++ {
+		cases[idx] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(consumes[idx])}
+	}
+loop:
+	for {
+		chosen, recv, recvOk := reflect.Select(cases)
+		if chosen == 0 {
+			break loop
+		}
+		if recvOk {
+			hande := fns[chosen-1]
+			delivery := recv.Interface().(amqp.Delivery)
+			p.Get()
+			go func() {
+				defer p.Put()
+				hande(&delivery)
+			}()
+		} else {
+			return ErrorChanRecvFail
+		}
+	}
+	return nil
+}
+
+type SubscribeOption struct {
+	AutoAck   bool
+	Exclusive bool
+	NoLocal   bool
+	NoWait    bool
+	Args      amqp.Table
+	Hande     Handle
+}
+
+func NewSUbscribeDefaultOption(handle Handle) *SubscribeOption {
+	return &SubscribeOption{
+		AutoAck:   false,
+		Exclusive: false,
+		NoLocal:   false,
+		NoWait:    false,
+		Args:      amqp.Table{},
+		Hande:     handle,
+	}
+}
+
+func NewSubscriber(conn *amqp.Connection, ctx context.Context) *subscriber {
+	return &subscriber{
+		conn:   conn,
+		ctx:    ctx,
+		queues: make(map[string]*SubscribeOption),
+		uuid:   uuid.New().String(),
+	}
+}
